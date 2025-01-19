682275e9e78d8d9f7f8a8d1e1af4c3ae73b1671c
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-18 19:00:26 -0500

    listening to get requests

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+app.get('/', (req, res) => {
+    console.log('Yay I hit an endpoint', req.method)
+    res.sendStatus(201)
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('Ohhh now I hit the /dashboard endopint')
+    res.send('hi')
+})
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
