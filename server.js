3f57d8bcf1927e1eabe5c59c8f54436305522cff
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-18 19:11:42 -0500

    creating 2 types of endpoints

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+let data = {
+    name: 'james'
+}
+
+// Type 1 - Website Endpoints
+
 app.get('/', (req, res) => {
-    res.send('<h1>this is actually a website (html code)</h1><input/>')
+    res.send('<h1>Homepage</h1>')
 })
 
 app.get('/dashboard', (req, res) => {
-    console.log('Ohhh now I hit the /dashboard endopint')
-    res.send('hi')
+    res.send('<h1>Dashboard</h1>')
 })
+
+// Type 2 - API Endpoints (non visual)
+
+app.get('/api/data', (req, res) => {
+    console.log('This one was for data')
+    res.send(data)
+})
+
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
